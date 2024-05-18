b6996bfa54da5ef770397b772384be3bd7107144
ooyuo <doyounjeong0@gmail.com>
2024-05-18 22:28:35 +0900

    fix: formatting issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from './assets/react.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
